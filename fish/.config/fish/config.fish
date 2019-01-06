f3a07609f9e61834a04578e9fed676bec7575b98
viktorstrate <viktorstrate@gmail.com>
2019-01-06 18:22:22 +0100

    Change fish configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,21 @@
+# Fish shell related
+
+function prompt_git_branch
+    set -l branch (
+        begin
+            git symbolic-ref HEAD; or \
+            git rev-parse --short HEAD
+        end ^/dev/null | sed -e 's|^refs/heads/||'
+    )
+
+    if test $branch
+        echo -n -s (set_color brred)
+        echo -n -s $branch ' '
+    end
+end
+
+set fish_color_cwd_root 'red'
+
 function fish_prompt --description 'Write out the prompt'
 	set -l color_cwd
     set -l suffix
@@ -22,7 +40,14 @@ function fish_prompt --description 'Write out the prompt'
         set success "red"
     end
 
-    echo -n -s (set_color normal) "$USER" ' ' (set_color $color_cwd) (prompt_pwd) (set_color $success) " $suffix "
+    # echo -n -s (set_color normal) "$USER "
+    echo -n -s (set_color $color_cwd) (prompt_pwd) ' '
+    echo -n -s (prompt_git_branch)
+    echo -n -s (set_color $success) "$suffix "
+end
+
+function fish_greeting
+    echo -s "Welcome " (whoami)
 end
 
 set os (uname)
